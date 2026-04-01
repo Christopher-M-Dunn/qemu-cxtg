0d6831f3ab458dc7c90ab511fa1d91ffcfa51e6a
Christopher-M-Dunn <christopher.m.dunn@gmail.com>
2026-04-01 07:17:22 +0000

    updated logic on status flags to match spec TODO: OF,OP, and CU flags

--- target/riscv/cx_helper.c
+++ target/riscv/cx_helper.c
@@ -11,57 +11,62 @@
 target_ulong HELPER(cx_reg)(CPURISCVState *env, target_ulong cf_id, 
                              target_ulong rs1, target_ulong rs2)
 {
-    
-    // not sure if these are the right error values to set it to
-    if (env->mcx_selector == CX_INVALID_SELECTOR) {
-        cx_status_t cx_status = {.idx = env->cx_status};
-        cx_status.sel.IV = 1;
-        cx_status.sel.IC = 1;
-        cx_status.sel.IS = 1;
-        env->cx_status = cx_status.idx;
-        return (target_ulong)-1;
-    }
-
-    if (GET_CX_CXE(env->mcx_selector) == 1) {
-        riscv_raise_exception(env, RISCV_EXCP_ILLEGAL_INST, GETPC());
-    }
-
     uint32_t OPCODE_ID = cf_id;
     int32_t OPA = rs1;
     int32_t OPB = rs2;
+    uint32_t CX_CXE = GET_CX_CXE(env->mcx_selector);
     uint32_t CX_ID = GET_CX_ID(env->mcx_selector);
     uint32_t STATE_ID = GET_CX_STATE(env->mcx_selector);
     uint32_t VERSION = GET_CX_VERSION(env->mcx_selector);
-
-    if (VERSION != 1) {
-        cx_status_t cx_status = {.idx = env->cx_status};
-        cx_status.sel.IV = 1;
-        env->cx_status = cx_status.idx;
-    }
-
-    // stateless 
-    if (STATE_ID > 0 && num_states[CX_ID] == 0) {
-        cx_status_t cx_status = {.idx = env->cx_status};
-        cx_status.sel.IS = 1;
-        env->cx_status = cx_status.idx;
-    }
-    // stateful
-    else if (STATE_ID > num_states[CX_ID] - 1) {
-        cx_status_t cx_status = {.idx = env->cx_status};
-        cx_status.sel.IS = 1;
-        env->cx_status = cx_status.idx;
-    }
-
-    if (OPCODE_ID >= MAX_CF_IDS || cx_funcs[CX_ID][OPCODE_ID] == NULL) {
-        cx_status_t cx_status = {.idx = env->cx_status};
-        cx_status.sel.IF = 1;
-        env->cx_status = cx_status.idx;
-    }
     
-    assert( CX_ID < MAX_CX_ID); // Possibly redundant
+    cx_status_t cx_status = {.idx = env->cx_status};
+   
+    int32_t out = -1;
 
-    cx_selidx_t sys_sel = {.idx = env->mcx_selector};
-    int32_t out = cx_funcs[CX_ID][OPCODE_ID](OPA, OPB, sys_sel);
+    // not sure if these are the right error values to set it to
+    if (env->mcx_selector == CX_INVALID_SELECTOR) {
+        cx_status.sel.IV = 1;
+        cx_status.sel.IC = 1;
+        cx_status.sel.IS = 1;
+    }
 
+    else if (CX_CXE == 1) {
+        riscv_raise_exception(env, RISCV_EXCP_ILLEGAL_INST, GETPC());
+    }
+
+    else if (VERSION != 1) {
+        cx_status.sel.IV = 1;
+    }
+
+    else if (CX_ID > MAX_CX_ID - 1) {
+        cx_status.sel.IC = 1;
+    }
+ 
+    else if (STATE_ID > (num_states[CX_ID] == 0 ? 0 : num_states[CX_ID] - 1) ) {
+        cx_status.sel.IS = 1;
+    }
+
+    //    else if (){
+    //        //TODO: OF flag
+    //    }
+
+    else if (OPCODE_ID >= MAX_CF_IDS || cx_funcs[CX_ID][OPCODE_ID] == NULL) {
+        cx_status.sel.IF = 1;
+    }
+
+    else { // no errors
+        cx_selidx_t sys_sel = {.idx = env->mcx_selector};
+        out = cx_funcs[CX_ID][OPCODE_ID](OPA, OPB, sys_sel);
+
+        //    if (){
+        //        //TODO: OP flag
+        //    }
+
+        //    else if (){
+        //        //TODO: CU flag
+        //    }
+    }
+
+    env->cx_status = cx_status.idx;
     return (target_ulong)out;
 } 
